67c006a05fcadf96c1bd652dada4b46a990e9e4c
Jesus Guio <jfranciscoguio@protonmail.com>
2021-08-20 20:14:05 -0500

    Added doxygen comments to Buffer and Mesh classes

--- source/assets/Mesh.hpp
+++ source/assets/Mesh.hpp
@@ -17,8 +17,14 @@ namespace re {
     class Device;
     class Buffer;
 
+    /**
+     * @brief Mesh Class
+     */
     class Mesh : NonCopyable {
     public:
+        /**
+         * @brief Vertex class
+         */
         struct Vertex {
             vec3 position;
             vec3 color;
@@ -27,28 +33,60 @@ namespace re {
 
             bool operator==(const Vertex &other) const;
 
+            /**
+             *
+             * @return Vector with the Vulkan Vertex Input Binding Description of Vertex
+             */
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 
+            /**
+             *
+             * @return Vector with the Vulkan Input Attributes Description of Vertex
+             */
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
+        /**
+         * @brief Vertex create Data. Vertices and indices
+         */
         struct Data {
             std::vector<Vertex> vertices;
             std::vector<uint32_t> indices;
         };
 
     public:
+        /**
+         * Default constructor
+         * @param device shared_ptr of Device
+         * @param data Data object
+         */
         Mesh(std::shared_ptr<Device> device, const Data& data);
 
         ~Mesh() override;
 
+        /**
+         * @brief Bind mesh
+         * @param commandBuffer Command buffer in recording state
+         */
         void bind(VkCommandBuffer commandBuffer);
 
+        /**
+         * @brief Draw mesh
+         * @param commandBuffer Command buffer in recording state
+         */
         void draw(VkCommandBuffer commandBuffer) const;
 
     private:
+        /**
+         * @brief Create vertex buffer in GPU only use
+         * @param vertices vector of Vertex
+         */
         void createVertexBuffer(const std::vector<Vertex>& vertices);
 
+        /**
+         * @brief Create index buffer in GPU only use
+         * @param indices vector of unsigned int
+         */
         void createIndexBuffer(const std::vector<uint32_t>& indices);
 
     private:
--- source/render/Buffer.hpp
+++ source/render/Buffer.hpp
@@ -11,21 +11,50 @@
 
 namespace re {
 
+    /**
+     * @brief Buffer class
+     */
     class Buffer {
     public:
+        /**
+         * @brief Default constructor
+         * @param allocator Vulkan Memory Allocator
+         * @param size Buffer size
+         * @param usageFlags Vulkan buffer usage flags
+         * @param memoryUsage Vulkan Memory Allocator memory usage
+         */
         Buffer(VmaAllocator allocator, VkDeviceSize size, VkBufferUsageFlags usageFlags, VmaMemoryUsage memoryUsage);
 
         ~Buffer();
 
+        /**
+         * @brief Map buffer using Vulkan Memory Allocator
+         */
         void map();
 
+        /**
+         * @brief Copy data to mapped pointer
+         * @tparam T Data type
+         * @param data Raw pointer of data
+         */
         template<typename T>
         void copyTo(T* data) const;
 
+        /**
+         * @brief Unmap buffer
+         */
         void unmap();
 
+        /**
+         *
+         * @return Raw vulkan buffer
+         */
         [[nodiscard]] const VkBuffer& getBuffer() const;
 
+        /**
+         *
+         * @return Vulkan buffer size
+         */
         [[nodiscard]] VkDeviceSize getSize() const;
 
     private:
