4cdce56a93aae0679008ea5ff617dae3cef83a2b
Jesus Guio <jfranciscoguio@protonmail.com>
2021-09-01 13:43:28 -0500

    Gltf sample shaders added

--- samples/gltf/Gltf.cpp
+++ samples/gltf/Gltf.cpp
@@ -8,9 +8,9 @@
 
 Gltf::Gltf() : re::Base("Gltf") {
     scene->loadScene("scenes/gltf.json", assetsManager.get());
-    renderSystem = std::make_unique<re::RenderSystem>(renderer->getDevice(), renderer->getRenderPass(), "samples/cube/model", *scene);
+    renderSystem = std::make_unique<re::RenderSystem>(renderer->getDevice(), renderer->getRenderPass(), "samples/gltf/model", *scene);
 
-    suzanne = scene->getEntity("Suzanne");
+    entity = scene->getEntity("Damaged Helmet");
 }
 
 Gltf::~Gltf() = default;
@@ -23,14 +23,14 @@ void Gltf::onDrawImGui() {
     ImGui::SetNextWindowSize({-1, -1});
     ImGui::Begin("Debug Window");
     {
-        ImGui::Text("Entity name %s", suzanne->getName().c_str());
+        ImGui::Text("Entity name %s", entity->getName().c_str());
 
-        auto& transform = suzanne->getComponent<re::Transform>();
-        ImGui::InputFloat3(fmt::format("{} Position", suzanne->getName()).c_str(), transform.position.ptr());
-        ImGui::InputFloat3(fmt::format("{} Size", suzanne->getName()).c_str(), transform.scale.ptr());
+        auto& transform = entity->getComponent<re::Transform>();
+        ImGui::InputFloat3(fmt::format("{} Position", entity->getName()).c_str(), transform.position.ptr());
+        ImGui::InputFloat3(fmt::format("{} Size", entity->getName()).c_str(), transform.scale.ptr());
 
         re::quat rotation = transform.rotation;
-        ImGui::DragFloat4(fmt::format("{} Rotation", suzanne->getName()).c_str(), rotation.ptr(), 0.1f);
+        ImGui::DragFloat4(fmt::format("{} Rotation", entity->getName()).c_str(), rotation.ptr(), 0.1f);
         rotation.normalise();
         transform.rotation = rotation;
     }
