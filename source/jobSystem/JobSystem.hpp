a47aef008b78594aacad4c00c5b5cea160a5ec95
Jesus Guio <jfranciscoguio@protonmail.com>
2021-09-05 09:24:05 -0500

    Doxygen comments to JobSystem added

--- source/jobSystem/JobSystem.hpp
+++ source/jobSystem/JobSystem.hpp
@@ -21,12 +21,25 @@ namespace re {
         using Job = std::function<void()>;
 
     public:
+        /**
+         * @brief Setup Thread with the num of core available - 1(main thread)
+         */
         static void setUp();
 
+        /**
+         * @brief Joint threads
+         */
         static void stop();
 
+        /**
+         * @brief Submit job to queue
+         * @param job void function without parameters
+         */
         static void submit(Job job);
 
+        /**
+         * @brief Check if jobs queue is empty
+         */
         static bool empty();
 
     private:
